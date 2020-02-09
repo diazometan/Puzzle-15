9c82c5517eba65fa7a8d9d918cbde0291b044655
diazometan <aleks3i.filat@yandex.ru>
2020-02-10 00:10:09 +0300

    add local storage

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const resetButtonElem = document.querySelector('.button_type_reset');
 const startButtonElem = document.querySelector('.button_type_start');
 const timerValueElem = document.querySelector('.statistics__timer-value');
 const playerNameElem = document.querySelector('.statistics__player-name');
+const winnerList = document.querySelector('.statistics__winners-list');
 const form = document.forms.new;
 
 let timerId;
-
 let cells = [];
+
+let playersArray = localStorage.getItem('players') ?
+    JSON.parse(localStorage.getItem('players')) : []
+
+localStorage.setItem('players', JSON.stringify(playersArray))
 
 /* Инициализация игры */
 for (let i = 1; i < 16; i++) {
@@ -16,30 +21,48 @@ for (let i = 1; i < 16; i++) {
 cells.push(0);
 
 renderCells();
+renderWinners();
+
+/* ---- */
 
 /* Обработчики */
 
+/**
+ * Обработчик ввода имени в форму
+ * @return {void}
+ */
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     stopGame();
     clearTimer();
 
     const playerName = form.elements.name;
-
     playerNameElem.textContent = playerName.value;
 });
 
+/**
+ * Обработчик кнопки Reset
+ * @return {void}
+ */
 resetButtonElem.addEventListener('click', function () {
     stopGame();
     resetCells();
 });
 
+/**
+ * Обработчик кнопки Start
+ * @return {void}
+ */
 startButtonElem.addEventListener('click', function () {
     activateCells();
     activateTimer();
     boxElem.addEventListener('click', cellClickHandler);
 });
 
+/**
+ * Обработчик нажатия на ячейку
+ * @return {void}
+ */
 
 function cellClickHandler(e) {
     const targetCell = e.target.closest('.cell');
@@ -54,30 +77,24 @@ function cellClickHandler(e) {
         return;
     }
 
-    // ячейка может двигаться: обновляем массив и страницу
-    const numIdx = cells.indexOf(num);
-    const zeroIdx = cells.indexOf(0);
-    cells[numIdx] = 0;
-    cells[zeroIdx] = num;
-
-    const zeroCell = document.querySelector('[id="0"]');
-    repaintCell(targetCell, 0);
-    repaintCell(zeroCell, num);
+    swapCell(targetCell, num);
 
     const isGameOver = checkGame();
-
     if (!isGameOver) {
         return;
     }
 
-    stopGame();
-    addWinnerToList();
-
-    setTimeout(function () {
-        alert('You win! Game over!');
-    }, 0);
+    EndGame();
 }
 
+/* ---- */
+
+/* Функции */
+
+/**
+ * Функция, которая оканчивает игру, делая ячейки неактивными
+ * @return {void}
+ */
 function stopGame() {
     boxElem.removeEventListener('click', cellClickHandler);
 
@@ -89,46 +106,121 @@ function stopGame() {
     });
 }
 
-/* Проверка закончилась ли игра */
+/**
+ * Проверка закончилась ли игра
+ * @return {void}
+ */
 function checkGame() {
     return cells.slice(0, 15).every(function (v, i) {
         return v === i + 1;
     });
 }
 
-/* Добавляет победителя в список */
-function addWinnerToList() {
-    const winnerList = document.querySelector('.statistics__winners-list');
+/**
+ * Меняем выбранную ячейку со свободной
+ * @return {void}
+*/
+function swapCell(targetCell, num) {
+    const numIdx = cells.indexOf(num);
+    const zeroIdx = cells.indexOf(0);
+    cells[numIdx] = 0;
+    cells[zeroIdx] = num;
 
-    const winner = createElem('li', 'statistics__winners-item');
-    const playerName = form.elements.name;
-    winner.textContent = playerName.value + ' - ' + timerValueElem.textContent + ' sec';
-
-    winnerList.appendChild(winner);
+    const zeroCell = document.querySelector('[id="0"]');
+    repaintCell(targetCell, 0);
+    repaintCell(zeroCell, num);
 }
 
-function addWinnerToLocalStorage() {
-    //localStorage.
+/**
+ * Окончания игры
+ * @return {void}
+*/
+function EndGame() {
+    stopGame();
+    addWinnerToList();
+
+    setTimeout(function () {
+        alert('You win! Game over!');
+    }, 0);
+}
+
+/**
+ * Сортирует победителей по имени
+ * Возвращает 1, если первый элемент больше второго лексикографически
+ * Возвращает -1, если второй элемент больше первого лексикографически
+ * Иначе 0
+ * @return {Number}
+ */
+function compareWinnersByName(a, b) {
+    if (a < b) {
+        return -1;
+    }
+    else if (a > b) {
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * Сортирует победителей
+ * Возвращает 1, если время первого элемент больше второго лексикографически
+ * Возвращает -1, если время второго элемент больше первого лексикографически
+ * Иначе сравнивает по имени
+ * @return {Number}
+ */
+function compareWinners(a, b) {
+    nameFirst = a.substring(0, a.indexOf('-'));
+    nameSecond = b.substring(0, b.indexOf('-'));;
+    a = a.match(/\d+:[0-5][0-9]/);
+    b = b.match(/\d+:[0-5][0-9]/);
+
+    if (a < b) {
+        return -1;
+    }
+    else if (a > b) {
+        return 1;
+    }
+
+    return compareWinnersByName(nameFirst, nameSecond);
+}
+
+/**
+ * Добавляет победителя в список
+ * @return {void}
+ */
+function addWinnerToList() {
+    const winner = createElem('li', ['statistics__winners-item']);
+    winner.textContent = playerNameElem.textContent + ' - ' + timerValueElem.textContent + ' sec';
+
+    playersArray.push(winner.textContent);
+    localStorage.setItem('players', JSON.stringify(playersArray));
+
+    renderWinners();
+
+    form.reset();
 }
 
 /**
  * Создает элемент
  * @param {String} elementType - тип DOM элемента
- * @param {String} elementClass - классы, который нужно добавить
+ * @param {Array} elementClassArray - классы, который нужно добавить
  * @return {Object}
  */
-function createElem(elementType, elementClass) {
+function createElem(elementType, elementClassArray) {
+    let elementClass = elementClassArray.join(' ');
     const elem = document.createElement(elementType);
     elem.classList.add(elementClass);
     return elem;
 }
 
-/*
+/**
  * Проверка ячейки в массиве: может ли она двигаться?
  * Сells: границы массива 0 и 15
  * проверяем ячейки сверху, снизу, слева, справа
  *  - проверить не выходим ли мы за диапазон массива (будет ошибка)
  *  - если находим 0 --> ячейка может двигаться
+ * @return {void}
  */
 function checkCell(n) {
     const idx = cells.indexOf(n);
@@ -160,7 +252,10 @@ function checkCell(n) {
     return false;
 }
 
-/* Активация ячеек */
+/**
+ * Активация ячеек
+ * @return {void}
+ */
 function activateCells() {
     const cellElems = document.querySelectorAll('.cell');
     cellElems.forEach(function (el) {
@@ -168,7 +263,10 @@ function activateCells() {
     });
 }
 
-/* Активация таймера */
+/**
+ * Активация таймера
+ * @return {void}
+ */
 function activateTimer() {
     clearTimer();
 
@@ -183,7 +281,10 @@ function activateTimer() {
     }, 1000);
 }
 
-/* Отчистка таймера */
+/**
+ * Отчистка таймера
+ * @return {void}
+ */
 function clearTimer() {
     clearInterval(timerId);
     timerValueElem.textContent = '00:00';
@@ -206,7 +307,6 @@ function resetCells() {
 
 /**
  * Рендерит ячейки
- *
  * @return {void}
  */
 function renderCells() {
@@ -222,6 +322,29 @@ function renderCells() {
 }
 
 /**
+ * Рендерит победителей
+ * @return {void}
+ */
+function renderWinners() {
+    while (winnerList.hasChildNodes()) {
+        winnerList.removeChild(winnerList.lastChild);
+    }
+
+    const winnerTemplate = document.querySelector('[data-component="statistics__winners-item"]');
+
+    playersArray.sort(compareWinners);
+
+    playersArray.forEach(function (player) {
+        const winnerFragment = winnerTemplate.content.cloneNode(true);
+        const winnerElem = winnerFragment.querySelector('.statistics__winners-item');
+
+        winnerElem.textContent = player;
+
+        winnerList.appendChild(winnerElem);
+    })
+}
+
+/**
  * Обновляет ячейку
  * @param {Object} cellElem - элемент ячейки
  * @param {Number} n - число для записи в ячейку
@@ -232,3 +355,5 @@ function repaintCell(cellElem, n) {
     cellElem.id = n;
     cellNumElem.textContent = n;
 }
+
+/* ---- */
